b1a3cadacf76ab53cd84d5f024687a7ec4aaedd3
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-08-08 10:38:57 +0530

    Finished the styling of the mobile design for the Layout component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,20 +1,25 @@
+import { Link } from "react-router-dom"
 import { BiLogoInstagramAlt } from "react-icons/bi";
 import { FaTwitter } from "react-icons/fa";
 import { LiaBehance } from "react-icons/lia";
 import { PiLinkedinLogoFill } from "react-icons/pi";
 import logo from "../assets/logo.svg"
 import menu from "../assets/menu.svg"
+import arrow from "../assets/menu-down.svg"
 
 export default function Layout() {
   return (
-    <>
-        <nav>
-            <img src={logo} alt="Mufteey Logo" />
+    <div className="flex flex-col">
+        <nav className="px-6 py-4 flex justify-between">
+            <Link to="/">
+                <img src={logo} alt="Mufteey Logo" />
+            </Link>
             <img src={menu} alt="Open menu" />
         </nav>
-        <footer>
+
+        <footer className="absolute bottom-0 flex flex-col justify-center mt-auto pt-6 pb-3 px-7 w-full text-center text-white text-[6.24px] bg-black">
             <p>Mufteey&apos;s Design</p>
-            <div>
+            <div className="self-center flex justify-between mt-4 w-full max-w-[135px] text-base">
                 {/*Socials Image Links*/}
                 <a href=""><BiLogoInstagramAlt/></a>
                 <a href=""><FaTwitter /></a>
@@ -22,17 +27,14 @@ export default function Layout() {
                 <a href=""><LiaBehance /></a>                
             </div>
             <div>
-                <div>
-                    
-                </div>
-                <div>
-                    <select name="language">
+                <div className="mt-4 flex justify-between">
+                    <select className={`appearance-none pr-3 bg-transparent bg-[url('.${arrow}')] bg-right bg-no-repeat`} name="language">
                         <option value="English">English</option>
                     </select>
                     <p>2024 Mufteey. All right reserved</p>
                 </div>
             </div>
         </footer>
-    </>
+    </div>
   )
 }
