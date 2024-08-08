8da0c91f7531aaa84297d807dc58cfdcb4fc4f67
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-08-08 12:56:08 +0530

    Added the basic markup for the home page

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import './index.css'
 import Layout from './components/Layout'
+import Index from './pages/Home'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />}>
-        <Route index element={<h1>Hello World & UI/UX Project</h1>}/>
+        <Route index element={<Index />}/>
     </Route>
   )
 )
---
+++ src/pages/Home.jsx
@@ -0,0 +1,45 @@
+export default function Home() {
+  return (
+    <section>
+        <div>
+            <h1>My name is Mustapha Danladi</h1>
+            <p>A UI/UX Designer currently designing thoughtful experience, beautiful websites, products, and design system to help designers, product managers and engineers to collaborate together.</p>
+
+            <button>Let&apos;s Talke</button>
+        </div>
+
+        <div>
+            <h1>My Services</h1>
+            <p>Transforming visions into captivating realities where design meets innovation for an exceptional UI/UX journey.</p>
+            {/* Services Card */}
+        </div>
+
+        <div>
+            <h1>My Portfolio Projects</h1>
+            <p>Explore the artistry and precision in my portfolio projects</p>
+            {/* Project Cards */}
+        </div>
+
+        <div>
+            <p>Redesign your Existing website with me.</p>
+            <button>Get In Touch</button>
+        </div>
+
+        <div>
+            <h1>Testimonial</h1>
+            <p>Testimonials Speak louder than words.</p>
+            {/* Gallery Component */}
+            <div>
+                <p>Hey! Take a look at my resume.</p>
+                <button>Download Resume</button>
+            </div>
+        </div>
+
+        <div>
+            <h1>Get In Touch</h1>
+            <p>Let’s create something extraordinary- Get In Touch</p>
+            {/* Form Component */}
+        </div>
+    </section>
+  )
+}
