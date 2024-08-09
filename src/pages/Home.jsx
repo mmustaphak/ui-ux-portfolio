474c5576d7c8ba79bcfd0038a41e5411a1c9132e
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-08-09 16:12:52 +0530

    Added the styling for the 'My services' cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,10 +15,10 @@ export default function Home() {
             <h1 className="text-black font-semibold">My Services</h1>
             <p>Transforming visions into captivating realities where design meets innovation for an exceptional UI/UX journey.</p>
             {/* Services Card */}
-            <div>
-                <img src={figma} alt=" " />
-                <h2>User Research and Analysis</h2>
-                <p>Understanding user needs and behaviors through methods like surveys and interviews to inform design decisions and create user personas.</p>
+            <div className="mt-2 py-8 px-5 rounded-lg bg-whitish">
+                <img className="mx-auto" src={figma} alt=" " />
+                <h2 className="font-medium text-[13.3px] text-black mt-[18px]">User Research and Analysis</h2>
+                <p className="text-[13.3px] mt-2">Understanding user needs and behaviors through methods like surveys and interviews to inform design decisions and create user personas.</p>
             </div>
         </div>
 
