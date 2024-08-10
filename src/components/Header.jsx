809e2cda436a0368a30c55e02df70579f68b106a
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-08-10 09:13:42 +0530

    Added the basic markup for the hamburger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 import logo from "../assets/logo.svg"
 import menu from "../assets/menu.svg"
+import close from "../assets/close-hamburger.svg"
 
 export default function Header() {
+
     return (
         <header>
             <nav className="px-6 py-4 flex justify-between">
@@ -10,6 +12,26 @@ export default function Header() {
                     <img src={logo} alt="Mufteey Logo" />
                 </Link>
                 <img src={menu} alt="Open menu" />
+            </nav>
+
+            <nav>
+                <button>
+                    <img src={close} alt="Close " />
+                </button>
+                <ul>
+                    <NavLink to="about">
+                        <li>About</li>
+                    </NavLink>
+                    <NavLink to="services">
+                        <li>Services</li>
+                    </NavLink>
+                    <NavLink to="portfolio">
+                        <li>Portfolio</li>
+                    </NavLink>
+                    <NavLink to="blog">
+                        <li>Blog</li>
+                    </NavLink>
+                </ul>
             </nav>
         </header>
     )
