493713dfed4cfff08fb65973dac4ba49c4c14bf6
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-08-11 08:37:04 +0530

    Moved the 'My Porfolio Projects' section from the home page to the Porfolio Componenet

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,19 @@
+import projectImg from "../assets/portfolio-project.png"
+
+export default function Portfolio() {
+    return (
+        <>
+            <h1 className="text-black font-semibold">My Portfolio Projects</h1>
+            <p>Explore the artistry and precision in my portfolio projects</p>
+            {/* Project Cards */}
+            <a href="">
+                <div className="mt-4">
+                    <div className="p-7 rounded-lg bg-whitish">
+                        <img className="mx-auto" src={projectImg} alt="The Bee Charge Design Project" />
+                    </div>
+                    <h2 className="mt-3 font-semibold text-left text-black">The Bee Charge Design</h2>
+                </div>
+            </a>
+        </>
+    )
+}
