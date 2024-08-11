273e17f5236dc0c3accefb22acfe1caccd015f4c
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-08-11 08:43:25 +0530

    Added the padding to the portfolio route

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -3,7 +3,7 @@ import Resume from "../components/Resume"
 
 export default function Portfolio() {
     return (
-        <section>
+        <section className="pt-16 px-6 pb-6">
             <PortfolioComponent />
             <Resume />
         </section>
