e6eae08aaf1703195e166fb0bee14927b44093ad
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-08-12 09:31:51 +0530

    Added the footer logo

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,6 +5,7 @@ import { LiaBehance } from "react-icons/lia";
 import { PiLinkedinLogoFill } from "react-icons/pi";
 import Header from "./Header";
 import GetInTouchForm from "./GetInTouchForm";
+import logo from "../assets/footer-logo.svg"
 
 export default function Layout() {
 
@@ -12,7 +13,7 @@ export default function Layout() {
 
     return (
         <div className="flex flex-col text-center text-grey bg-[url('src/assets/background-logo.svg')] bg-[position:center_4%] bg-no-repeat">
-            
+
             <Header />
 
             <Outlet />
@@ -24,7 +25,10 @@ export default function Layout() {
             </div>
 
             <footer className="flex flex-col justify-center mt-auto pt-6 pb-3 px-7 w-full text-center text-white text-[6.24px] bg-black">
-                <p>Mufteey&apos;s Design</p>
+                <div className="flex mx-auto">
+                    <img className="mr-1" src={logo} alt=" " />
+                    <p>Mufteey&apos;s Design</p>
+                </div>
                 <div className="self-center flex justify-between mt-4 w-full max-w-[135px] text-base">
                     <a href=""><BiLogoInstagramAlt /></a>
                     <a href=""><FaTwitter /></a>
