60717ffafdeadbc99df1941d1bf039883e709af0
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-08-15 15:57:58 +0530

    Finished styling the footer component for larger screensizes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -26,26 +26,34 @@ export default function Layout() {
             </div>
 
 
-            <footer className="flex flex-col justify-center mt-auto pt-6 pb-3 px-7 w-full text-center text-white text-[6.24px] bg-black">
-                <div className="flex mx-auto">
-                    <img className="mr-1" src={logo} alt=" " />
-                    <p>Mufteey&apos;s Design</p>
-                </div>
-                <div className="self-center flex justify-between mt-4 w-full max-w-[135px] text-base">
-                    <a href=""><BiLogoInstagramAlt /></a>
-                    <a href=""><FaTwitter /></a>
-                    <a href=""><PiLinkedinLogoFill /></a>
-                    <a href=""><LiaBehance /></a>
-                </div>
-                <div>
-                    <div className="mt-4 flex justify-between">
-                        <select className={`appearance-none pr-3 bg-transparent bg-[url('./src/assets/menu-down.svg')] bg-right bg-no-repeat`} name="language">
-                            <option value="English">English</option>
-                        </select>
-                        <p>{year} Mufteey. All right reserved</p>
+            <footer className="mt-auto pt-6 pb-3 px-7 w-full text-center text-white text-[6.24px] bg-black min-[480px]:text-lg  min-[480px]:px-16 min-[480px]:pt-16 min-[480px]:pb-9 lg:px-[110px] lg:pt-[96px] lg:pb-[50px] lg:text-2xl">
+
+                <div className="flex flex-col justify-center mx-auto w-full max-w-[1220px]">
+                    <div className="flex mx-auto min-[480px]:min-w-[220px] lg:min-w-[300px]">
+                        <img className="mr-1 md:mr-2 lg:mr-3" src={logo} alt=" " />
+                        <p className="lg:text-2xl">Mufteey&apos;s Design</p>
                     </div>
+
+                    <div className="self-center flex justify-between mt-4 w-full max-w-[135px] text-base min-[480px]:mt-8 min-[480px]:max-w-[450px] lg:mt-[53px] lg:max-w-[552px]">
+                        <a href=""><BiLogoInstagramAlt className="min-[480px]:size-[25px] md:size-[30px] lg:size-[48px]" /></a>
+                        <a href=""><FaTwitter className="min-[480px]:size-[25px] md:size-[30px] lg:size-[48px]" /></a>
+                        <a href=""><PiLinkedinLogoFill className="min-[480px]:size-[25px] md:size-[30px] lg:size-[48px]" /></a>
+                        <a href=""><LiaBehance className="min-[480px]:size-[25px] md:size-[30px] lg:size-[48px]" /></a>
+                    </div>
+
+                    <div>
+                        <div className="mt-4 flex justify-between min-[480px]:mt-10 lg:mt-[63px]">
+                            <select className={`appearance-none pr-3 bg-transparent bg-[url('./src/assets/menu-down.svg')] bg-[position:center_right] bg-no-repeat min-[480px]:bg-[size:18px] min-[480px]:min-w-[5.5rem] lg:min-w-[7rem] lg:bg-[size:24px]`} name="language">
+                                <option value="English">English</option>
+                            </select>
+                            <p>{year} Mufteey. All right reserved</p>
+                        </div>
+                    </div>
+                    
                 </div>
+
             </footer>
+
         </div>
     )
 }
