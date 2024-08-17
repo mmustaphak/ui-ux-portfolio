27b67ed747ddc8d83c7969920ab057486e6bf495
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-08-17 11:15:21 +0530

    Fixed the alignment of the margin on the Form component

--- src/components/GetInTouchForm.jsx
+++ src/components/GetInTouchForm.jsx
@@ -5,8 +5,8 @@ figure out how to push the submit button to the left*/
 
 export default function GetInTouchForm() {
   return (
-    <Form className="p-4 flex flex-col mx-auto md:pt-6 lg:p-0 lg:w-[90%] lg:pt-8">
-      <label className="block font-medium text-left text-black lg:text-2xl">
+    <Form className="px-4 flex flex-col mx-auto md:my-6 lg:px-0 lg:my-[50px] lg:w-[90%]">
+      <label className="block font-medium text-left text-black md:mt-6 lg:text-2xl">
         Name <br />
         <input
           className="w-full py-2 px-4 rounded-[4.29px] font-normal bg-whitish focus:outline-none focus:border focus:border-black placeholder:font-normal md:min-h-[52px] lg:mt-4 lg:py-[27px] lg:px-10 lg:focus:border-3"
@@ -14,7 +14,7 @@ export default function GetInTouchForm() {
           placeholder="Enter your name here"
         />
       </label>
-      <label className="block mt-4 font-medium text-left text-black lg:mt-[92px] lg:text-2xl">
+      <label className="block mt-4 font-medium text-left text-black md:mt-6 lg:mt-8 lg:text-2xl">
         Email Address <br />
         <input
           className="w-full py-2 px-4 rounded-[4.29px] font-normal bg-whitish focus:outline-none focus:border focus:border-black placeholder:font-normal md:min-h-[52px] lg:mt-4 lg:py-[27px] lg:px-10 lg:focus:border-3"
@@ -22,7 +22,7 @@ export default function GetInTouchForm() {
           placeholder="Enter your name here"
         />
       </label>
-      <label className="block mt-4 font-medium text-left text-black lg:mt-[92px] lg:text-2xl">
+      <label className="block mt-4 font-medium text-left text-black md:mt-6 lg:mt-8 lg:text-2xl">
         Message <br />
         <textarea
           className="w-full min-h-[119px] resize-none py-2 px-4 rounded-[4.29px] font-normal bg-whitish focus:outline-none focus:border focus:border-black placeholder:font-normal md:min-h-[270px] lg:mt-4 lg:py-[27px] lg:px-10 lg:focus:border-3"
@@ -31,7 +31,7 @@ export default function GetInTouchForm() {
       </label>
 
       <button
-        className="self-start py-1 px-[52px] mt-4 font-medium rounded-[3.37px] text-[10.11px] text-white md:py-2 md:px-[90px] md:text-[18px] md:rounded-md bg-black md:font-semibold lg:pt-[10px] lg:px-[173px] lg:text-2xl lg:rounded-lg"
+        className="self-start py-1 px-[52px] mt-4 font-medium rounded-[3.37px] text-[10.11px] text-white md:py-2 md:px-[90px] md:text-[18px] md:rounded-md bg-black md:font-semibold md:mt-6 lg:mt-8 lg:pt-[10px] lg:px-[173px] lg:text-2xl lg:rounded-lg"
         type="submit"
       >
         Send
