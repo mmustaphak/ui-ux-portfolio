38ae86f32531957d1ee340d77fe39b4b27ad8dfb
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-08-20 12:31:05 +0530

    Added the destkop design for the about route without the background postion

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -15,7 +15,7 @@ export default function Layout() {
       <div className="max-w-[1220px] mx-auto">
         <Header />
 
-        <div className="pt-16 px-6 pb-6 bg-[url('src/assets/background-logo.svg')] bg-[size:min(89%,350px)] bg-[position:center_0.8%] min-[400px]:bg-[position:center_0.4%] md:bg-[size:min(55%,817px)] bg-no-repeat lg:bg-[position:center_4%] lg:bg-[size:817px_579px] md:px-8 min-[1250px]:px-0">
+        <div className="mt-16 px-6 mb-6 bg-[url('src/assets/background-logo.svg')] bg-[size:min(89%,350px)] bg-[position:center_0.8%] min-[400px]:bg-[position:center_0.4%] md:bg-[size:min(55%,817px)] bg-no-repeat lg:bg-[position:center_4%] lg:bg-[size:817px_579px] md:mb-[35px] md:px-8 lg:mb-[50px] min-[1250px]:px-0">
           <Outlet />
         </div>
 
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,10 +3,10 @@ import mufteey from "../assets/mufteey.png";
 
 export default function About() {
   return (
-    <section className="-mt-8">
-      <h1 className="font-semibold text-black">About Me</h1>
-      <img className="mx-auto pt-4" src={mufteey} alt="Mustapha Danladi" />
-      <p className="mt-6">
+    <section className="lg:mt-[151px]">
+      <h1 className="text-black font-semibold md:text-2xl lg:text-[2.5rem]">About Me</h1>
+      <img className="mx-auto mt-4 w-[29.4%] max-w-[160px] md:mt-6 lg:min-w-[329px] lg:mt-8" src={mufteey} alt="Mustapha Danladi" />
+      <p className="mt-6 text-[0.75rem] max-w-[512px] md:text-xl md:max-w-[760px] md:leading-[normal] lg:mt-8 lg:max-w-[1320px] lg:text-[2rem]">
         Hey there! I&apos;m Mufteey, a UI/UX designer driven by a passion for
         creating seamless and visually stunning digital experiences. With a keen
         eye for detail and a love for problem-solving, I specialize in
