5e21469a33f507601ca25f8462ad84b554e04b77
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-08-23 15:25:08 +0530

    Fixed the width of the porfolio route

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -12,7 +12,7 @@ export default function Layout() {
 
   return (
     <div className="flex flex-col text-center text-grey">
-      <div className="max-w-[1220px] mx-auto">
+      <div className="w-full max-w-[1220px] mx-auto">
         <Header />
 
         <div className="pt-16 px-6 mb-6 bg-[url('src/assets/background-logo.svg')] bg-[size:min(89%,350px)] bg-[position:center_16px] min-[400px]:bg-[position:center_top] md:bg-[size:55%] md:bg-[position:center_25px] lg:pt-[239px]  bg-no-repeat  lg:bg-[size:817px_579px] lg:bg-[position:center_80px] md:mb-[35px] md:px-8 lg:mb-[50px] min-[1250px]:px-0">
