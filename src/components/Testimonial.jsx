ce24483c308dbe182c969512b4dcf8d3fea8c3cb
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-09-01 16:21:50 +0530

    Added the functionality to scroll to desired testimonail on button click

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,46 +1,86 @@
+import { useRef } from "react"
+
 export default function Testimonial() {
+
+  const testimonialRef = useRef(null)
+
+
   const data = [
     {
-      person: "Alamaster",
+      name: "Alamaster",
       message:
         "I just wanted to share a quick note and let you know that you are doing  really good job. I'm glad I decided to work with you. It's really great how easy your websites are to update and manage.",
     },
     {
-      person: "Dev Ade",
+      name: "Dev Ade",
       message:
         "I pray you become the best version of yourself and also the biggest techie in Arewa and Naija in a whole insha Allah.",
     },
     {
-      person: "Techbro",
+      name: "Techbro",
       message:
         "You have some cool stuff here, i’ll send your work to my other guys to see what the outcome will be. Don’t stop designing bcos u really have some good concept.",
     },
-    { person: "Damilola", message: "This is impressive, i must commend you." },
+    { name: "Damilola", message: "This is impressive, i must commend you." },
     {
-      person: "GDG",
+      name: "GDG",
       message:
         "Thanks to you and your team for bringing your passion and creativity to the design of #DevFest",
     },
   ];
 
-  const renderedTestimonials = data.map((person,index) => (
+  function getMap() {
+    if (!testimonialRef.current) {
+      testimonialRef.current = new Map()
+    }
+    return testimonialRef.current
+  }
+
+  function scrollToTestimonial(testimonial) {
+    const map = getMap();
+    const node = map.get(testimonial)
+    node.scrollIntoView({
+      behavior: "smooth",
+      block:"nearest",
+      inline:"center",
+    })
+  }
+
+
+  const renderedTestimonials = data.map((testimonial, index) => (
     <div
-      key={person}
-      id={index}
-      className="min-w-[327px] bg-whitish p-5 mr-4 snap-center snap-always"
+      key={testimonial}
+      ref={node => {
+        const map = getMap();
+        if (node) {
+          map.set(testimonial.name, node);
+        } else {
+          map.delete(testimonial)
+        }
+      }}
+      className={`testimonial${index} min-w-[327px] bg-whitish p-5 mr-4 snap-center snap-always`}
     >
       <h3 className="font-semibold text-[13.3px] text-black mt-[18px] md:text-lg lg:mt-8 lg:text-[1.5rem]">
-        From {person.person}
+        From {testimonial.name}
       </h3>
       <p className="text-[11px] mt-2 lg:mt-4 lg:text-[1.25rem]">
-        {person.message}
+        {testimonial.message}
       </p>
     </div>
   ));
+
+  const navigationButtons = data.map(person => {
+    return (<button key={person} onClick={()=>scrollToTestimonial(person.name)} className="size-2 bg-[#D9D9D9] rounded-full"></button>)
+  })
+
+
   return (
     <>
       <div className="testimonial relative flex mt-2 overflow-x-scroll snap-x snap-mandatory">
         {renderedTestimonials}
+      </div>
+      <div className="flex justify-between mx-auto max-w-[75px]">
+        {navigationButtons}
       </div>
     </>
   );
