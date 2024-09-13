7e07504c7a0d6c1bbcc9316a0afa7860bf8415e8
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-09-13 08:49:35 +0530

    Added a reusalbe Paragraph Component

---
+++ src/components/Paragraph.jsx
@@ -0,0 +1,5 @@
+export default function Paragraph({children, ClassName}) {
+  return (
+    <p className={`${ClassName} text-[0.75rem] mx-auto max-w-[512px] md:max-w-[760px] md:text-xl md:leading-[normal] md:mt-4 lg:mt-8 lg:max-w-[1320px] lg:text-[2rem]`}>{children}</p>
+  )
+}
