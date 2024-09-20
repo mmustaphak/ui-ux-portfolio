659095f5ea38831ee595afa1e388942fd953c5a6
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-09-20 15:18:42 +0530

    created a themeProvdider in the ThemeContext.jsx file

--- src/components/ThemeContext.jsx
+++ src/components/ThemeContext.jsx
@@ -1,3 +1,11 @@
 import { createContext } from "react";
 
 export const ThemeContext = createContext(null);
+
+export function ThemeProvider({children}){
+    return(
+        <ThemeContext.Provider value={'light'}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
