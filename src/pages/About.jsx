360632688d82b42cd5e836a1a5bfecd7b17f7218
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-09-20 15:52:11 +0530

    Refactored the themeContext in the About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,10 +7,10 @@ import { ThemeContext } from "../components/ThemeContext";
 
 export default function About() {
 
-  const {color, text} = useContext(ThemeContext)
+  const theme = useContext(ThemeContext)
 
-  function imageTheme(hexCode){
-    switch(hexCode){
+  function imageTheme(hexCode) {
+    switch (hexCode) {
       case '#151515':
         return blackMufteey
       case '#00A070':
@@ -22,18 +22,18 @@ export default function About() {
 
   return (
     <section className="mt-6 min-[400px]:mt-0 lg:-mt-6">
-      <h1 className={`${text} font-semibold md:text-2xl lg:text-[2.5rem]`}>
+      <h1 style={{ color: theme }} className="font-semibold md:text-2xl lg:text-[2.5rem]">
         About Me
       </h1>
 
       <img
         className="mx-auto mt-4 w-[29.4%] max-w-[160px] md:mt-6 lg:min-w-[329px] lg:mt-8"
-        src={imageTheme(color)}
+        src={imageTheme(theme)}
         alt="Mustapha Danladi"
       />
 
       <p className="mt-6 mx-auto text-[0.75rem] max-w-[512px] md:text-xl md:max-w-[760px] md:leading-[normal] lg:mt-8 lg:max-w-[1320px] lg:text-[2rem]">
-        Hey there! I&apos;m <span className={`${text} font-semibold`}>Mufteey</span>, a UI/UX designer driven by a passion for
+        Hey there! I&apos;m <span style={{ color: theme }} className="font-semibold">Mufteey</span>, a UI/UX designer driven by a passion for
         creating seamless and visually stunning digital experiences. With a keen
         eye for detail and a love for problem-solving, I specialize in
         translating ideas into user-friendly designs. Let&apos;s craft something
