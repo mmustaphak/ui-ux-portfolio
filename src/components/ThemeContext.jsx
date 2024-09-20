6892ad38e31509d4dac51b3395b619a09642e015
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-09-20 15:40:56 +0530

    Changed the themes array to contain only the hexcode of the theme

--- src/components/ThemeContext.jsx
+++ src/components/ThemeContext.jsx
@@ -5,28 +5,8 @@ export const ThemeContext = createContext(null);
 export function ThemeProvider({ children }) {
 
     function randomTheme() {
-        const themes = [
-            {
-                // black Theme
-                background: "bg-[#151515]",
-                text: "text-[#151515]",
-                color: "#151515",
-            },
-            {
-                // green Theme
-                background: "bg-[#00A070]",
-                text: "text-[#00A070]",
-                color: "#00A070",
-            },
-            {
-                // blue Theme
-                background: "bg-[#000958]",
-                text: "text-[#000958]",
-                color: "#000958",
-            },
-        ]
-
-        const randomIndex = Math.floor(Math.random() * 3)
+        const themes = ["#151515", "#00A070", "#000958"] // black, green and blue theme
+            const randomIndex = Math.floor(Math.random() * 3)
         return themes[randomIndex]
     }
 
