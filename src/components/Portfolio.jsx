923e25b13d2fd6266892f1dcda2ee7de40495d2d
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-09-21 15:50:19 +0530

    used the images in the porfolio componenet

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -16,24 +16,81 @@ export default function Portfolio() {
         Explore the artistry and precision in my portfolio projects
       </p>
       {/* Project Cards */}
-      <a href="">
-        <div className="mt-4 w-fit">
-          <div className="p-7 rounded-lg bg-whitish">
-            <img
-              className="mx-auto md:mx-0 md:mr-auto"
-              src={projectImg}
-              alt="The Bee Charge Design Project"
-            />
+      <div>
+        <a href="">
+          <div className="mt-4 w-fit">
+            <div className="p-7 rounded-lg bg-whitish">
+              <img
+                className="mx-auto md:mx-0 md:mr-auto"
+                src={projectImg}
+                alt="The Bee Charge Design Project"
+              />
+            </div>
+            <h2
+              style={{ color: theme }}
+              className="mt-3 font-semibold text-left md:text-xl md:leading-[normal] md:mt-4 lg:mt-8 lg:text-[2rem]"
+            >
+              The Bee Charge Design
+            </h2>
           </div>
+        </a>
+        <a href="">
+          <div className="mt-4 w-fit">
+            <div className="p-7 rounded-lg bg-whitish">
+              <img
+                className="mx-auto md:mx-0 md:mr-auto"
+                src={projectImg2}
+                alt="The Bee Charge Design Project"
+              />
+            </div>
 
-          <h2
-            style={{ color: theme }}
-            className="mt-3 font-semibold text-left md:text-xl md:leading-[normal] md:mt-4 lg:mt-8 lg:text-[2rem]"
-          >
-            The Bee Charge Design
-          </h2>
-        </div>
-      </a>
+            <h2
+              style={{ color: theme }}
+              className="mt-3 font-semibold text-left md:text-xl md:leading-[normal] md:mt-4 lg:mt-8 lg:text-[2rem]"
+            >
+              The Tafemoni Design
+            </h2>
+          </div>
+        </a>
+
+        <a href="">
+          <div className="mt-4 w-fit">
+            <div className="p-7 rounded-lg bg-whitish">
+              <img
+                className="mx-auto md:mx-0 md:mr-auto"
+                src={projectImg3}
+                alt="The Bee Charge Design Project"
+              />
+            </div>
+
+            <h2
+              style={{ color: theme }}
+              className="mt-3 font-semibold text-left md:text-xl md:leading-[normal] md:mt-4 lg:mt-8 lg:text-[2rem]"
+            >
+              The Tuunder Charge Design
+            </h2>
+          </div>
+        </a>
+
+        <a href="">
+          <div className="mt-4 w-fit">
+            <div className="p-7 rounded-lg bg-whitish">
+              <img
+                className="mx-auto md:mx-0 md:mr-auto"
+                src={projectImg4}
+                alt="The Bee Charge Design Project"
+              />
+            </div>
+
+            <h2
+              style={{ color: theme }}
+              className="mt-3 font-semibold text-left md:text-xl md:leading-[normal] md:mt-4 lg:mt-8 lg:text-[2rem]"
+            >
+              The GIIP Design
+            </h2>
+          </div>
+        </a>
+      </div>
     </>
   );
 }
