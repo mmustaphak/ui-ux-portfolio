ecc804987f20ce5cb44833559ac175c01b915452
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-10-09 10:51:23 +0530

    Increased the size of the spinner at larger screen sizes

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -5,6 +5,6 @@ export default function Spinner() {
     const theme = useContext(ThemeContext)
 
     return (
-        <div style={{ borderColor: theme }} className="animate-[spinner_1s_infinite_0s_linear] mx-auto mt-4 size-4 border-x-2 border-t-2 rounded-full"></div>
+        <div style={{ borderColor: theme }} className="animate-[spinner_1s_infinite_0s_linear] mx-auto mt-4 size-4 border-x-2 border-t-2 rounded-full md:size-12"></div>
     )
 }
