ddcedbaa6fbb61b892f661eeaba99ef1447e4c48
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-10-11 15:34:03 +0530

    Added the handle click function

--- src/components/GetInTouchForm.jsx
+++ src/components/GetInTouchForm.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Form, useActionData } from "react-router-dom";
 import { ThemeContext } from "./ThemeContext";
 
@@ -21,9 +21,14 @@ export async function action({ request }) {
 }
 
 export default function GetInTouchForm() {
-  const formResponse = useActionData();
 
+  const [isClicked, setIsClicked] = useState(false)
+  const formResponse = useActionData();
   const theme = useContext(ThemeContext);
+
+  function handleClick() {
+    setIsClicked(true)
+  }
 
   return (
     <Form
@@ -75,6 +80,7 @@ export default function GetInTouchForm() {
       </label>
 
       <button
+        onClick={handleClick}
         style={{ background: theme }}
         className="py-0.5 px-[23px] w-full max-w-[110px] min-[375px]:max-w-[130px] mt-4 font-medium rounded-[3.37px] text-[10.11px] text-white disabled:opacity-50 md:py-2 md:text-[18px] md:rounded-md md:max-w-[280px] md:font-semibold md:mt-6 lg:mt-8 lg:pt-[10px] lg:text-2xl lg:rounded-lg"
         type="submit"
