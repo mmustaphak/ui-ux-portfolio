212d001490ecec1d70019c25f1aaa21e8fe1b94b
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-10-11 16:00:39 +0530

    Optionally rendered the white on the spinner through secondary prop

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,11 +1,11 @@
 import { useContext } from "react";
 import { ThemeContext } from "./ThemeContext";
-export default function Spinner() {
+export default function Spinner({secondary}) {
   const theme = useContext(ThemeContext);
 
   return (
     <div
-      style={{ borderColor: theme }}
+      style={{ borderColor: secondary ? "#FFFFFF" : theme }}
       className="animate-[spinner_1s_infinite_0s_linear] mx-auto mt-4 size-4 border-x-2 border-t-2 rounded-full md:size-12"
     ></div>
   );
