8482ddb68c84d5dce10f0462797ff928ea528e33
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-10-11 09:04:31 +0530

    Made the 'load more' show all projects

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -67,8 +67,8 @@ export default function Portfolio() {
               const recentProject = 6
               const allProject = projectData.length
 
-              const shownProjects = (location.pathname === "/portfolio" || isShown) ? 
-              projectData.slice(0, allProject): projectData.slice(0, recentProject)
+              const shownProjects = (location.pathname === "/portfolio" || isShown) ?
+                projectData.slice(0, allProject) : projectData.slice(0, recentProject)
 
               return shownProjects.map(
                 ({ projectName, imageUrl, projectLink }) => (
@@ -84,10 +84,15 @@ export default function Portfolio() {
           </Await>
         </div>
         {
-          location.pathname === "/" && 
+          ((location.pathname === "/" && isShown === false )) &&
           <button
-          style={{ backgroundColor: theme }}
-          className="mx-auto py-0.5 px-[23px] w-full max-w-[110px] min-[375px]:max-w-[130px] mt-4 font-medium rounded-[3.37px] text-[10.11px] text-white disabled:opacity-50 md:py-2 md:text-[18px] md:rounded-md md:max-w-[280px] md:font-semibold md:mt-6 lg:mt-8 lg:pt-[10px] lg:text-2xl lg:rounded-lg">Load More</button>}
+            onClick={()=>setIsShown(true)}
+            style={{ backgroundColor: theme }}
+            className="mx-auto py-0.5 px-[23px] w-full max-w-[110px] min-[375px]:max-w-[130px] mt-4 font-medium rounded-[3.37px] text-[10.11px] text-white disabled:opacity-50 md:py-2 md:text-[18px] md:rounded-md md:max-w-[280px] md:font-semibold md:mt-6 lg:mt-8 lg:pt-[10px] lg:text-2xl lg:rounded-lg"
+          >
+            Load More
+          </button>
+        }
       </Suspense>
     </>
   );
