6c050f60e90ac2d3e8312e2f8327bf79d10b3d48
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-11-08 19:18:29 +0530

    fixed the spacing of the load more button

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -91,7 +91,7 @@ export default function Portfolio() {
                     <button
                       onClick={() => setIsShown(true)}
                       style={{ backgroundColor: theme }}
-                      className="mx-auto py-0.5 px-[23px] w-full max-w-[110px] min-[375px]:max-w-[130px] mt-4 font-medium rounded-[3.37px] text-[10.11px] text-white disabled:opacity-50 md:py-2 md:text-[18px] md:rounded-md md:max-w-[280px] md:font-semibold md:mt-6 lg:mt-8 lg:pt-[10px] lg:text-2xl lg:rounded-lg"
+                      className="mx-auto col-span-full py-0.5 px-[23px] w-full max-w-[110px] min-[375px]:max-w-[130px] mt-4 font-medium rounded-[3.37px] text-[10.11px] text-white disabled:opacity-50 md:py-2 md:text-[18px] md:rounded-md md:max-w-[280px] md:font-semibold md:mt-6 lg:mt-8 lg:pt-[10px] lg:text-2xl lg:rounded-lg"
                     >
                       Load More
                     </button>
