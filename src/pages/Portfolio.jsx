d65de4d152f1f839414ac2794fef9c6a1f9d8d91
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-12-08 09:00:43 +0530

    Derived the Portfolio.jsx loader into the Porfolio Page Loader

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,10 +1,20 @@
 import PortfolioComponent from "../components/Portfolio";
 import Resume from "../components/Resume";
+import { loader as portfolioComponentLoader } from "../components/Portfolio";
+import { useLoaderData } from "react-router-dom";
+
+export function loader(){
+  return portfolioComponentLoader()
+}
+
 
 export default function Portfolio() {
+
+  const { projectPromise } = useLoaderData();
+
   return (
     <section>
-      <PortfolioComponent />
+      <PortfolioComponent projectPromise={projectPromise}/>
       <Resume />
     </section>
   );
