19e457ecde9f48cc53531683550eb09bb39a1742
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-12-11 11:50:07 +0530

    Fixed the error of the testimonial component

--- src/components/Testimonial/NavButton.jsx
+++ src/components/Testimonial/NavButton.jsx
@@ -23,6 +23,7 @@ export default function NavButton({ index, currentTestimonial, testimonialMap })
           onClick={() => scrollToTestimonial(index)}
           className="size-2 bg-[#D9D9D9] rounded-full appearance-none hover:min-w-[15px] hover:checked:min-w-[23px] checked:min-w-[23px] checked:transition checked:duration-200 checked:ease-in-out md:size-3 md:hover:min-w-[30px] md:checked:min-w-[44px] lg:size-4 lg:hover:min-w-[55px] lg:checked:min-w-[72px]"
           checked={index == currentTestimonial}
+          readOnly={true}
         />
       </>
     );
