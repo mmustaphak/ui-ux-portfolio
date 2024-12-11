a37e3d4693b47f0bd18a8a48ff33f11ba864fc4e
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-12-11 11:08:10 +0530

    Moved the NavButton component to a sepearte file and folder

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,6 +1,7 @@
 import { useContext, useRef, useState } from "react";
 import { ThemeContext } from "./ThemeContext";
 import testimonialData from "../testimonial.json"
+import NavButton from "./Testimonial/NavButton";
 
 export default function Testimonial() {
   const testimonialRef = useRef(null);
@@ -23,6 +24,7 @@ export default function Testimonial() {
       inline: "center",
     });
   }
+
 
   const observer = new IntersectionObserver(handleIntersection, {
     root: document.querySelector("#scrollArea"),
@@ -65,27 +67,18 @@ export default function Testimonial() {
     );
   }
 
-  function NavButton({ index }) {
-    return (
-      <>
-        <style>{`:checked{background: ${theme}}`}</style>
-        <input
-          name="testimonials"
-          type="radio"
-          onClick={() => scrollToTestimonial(index)}
-          className="size-2 bg-[#D9D9D9] rounded-full appearance-none hover:min-w-[15px] hover:checked:min-w-[23px] checked:min-w-[23px] checked:transition checked:duration-200 checked:ease-in-out md:size-3 md:hover:min-w-[30px] md:checked:min-w-[44px] lg:size-4 lg:hover:min-w-[55px] lg:checked:min-w-[72px]"
-          checked={index == onScreenTestimonial}
-        />
-      </>
-    );
-  }
 
   const renderedTestimonials = testimonialData.map((testimonial, index) => (
     <Testimonial key={testimonial.name} {...{ ...testimonial, index }} />
   ));
 
   const navigationButtons = testimonialData.map((person, index) => (
-    <NavButton key={index} index={index} />
+    <NavButton
+      key={index}
+      handleScroll={scrollToTestimonial}
+      currentTestimonial={onScreenTestimonial}
+      index={index}
+    />
   ));
 
   return (
