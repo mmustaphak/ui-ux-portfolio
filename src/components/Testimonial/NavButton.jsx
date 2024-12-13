3b603f7a30f1fcc4fdbdc1b64810649622c75ed6
Mustapha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2024-12-13 18:40:50 +0530

    Made the testimonial naviagation a lot smoother

--- src/components/Testimonial/NavButton.jsx
+++ src/components/Testimonial/NavButton.jsx
@@ -2,29 +2,29 @@ import { useContext } from "react";
 import { ThemeContext } from "../ThemeContext";
 
 export default function NavButton({ index, currentTestimonial, testimonialMap }) {
-    const theme = useContext(ThemeContext)
+  const theme = useContext(ThemeContext)
 
-    function scrollToTestimonial(testimonial) {
-      const map = testimonialMap();
-      const node = map.get(testimonial);
-      node.scrollIntoView({
-        behavior: "smooth",
-        block: "nearest",
-        inline: "center",
-      });
-    }
-  
-    return (
-      <>
-        <style>{`:checked{background: ${theme}}`}</style>
-        <input
-          name="testimonials"
-          type="radio"
-          onClick={() => scrollToTestimonial(index)}
-          className="size-2 bg-[#D9D9D9] rounded-full appearance-none hover:min-w-[15px] hover:checked:min-w-[23px] checked:min-w-[23px] checked:transition checked:duration-200 checked:ease-in-out md:size-3 md:hover:min-w-[30px] md:checked:min-w-[44px] lg:size-4 lg:hover:min-w-[55px] lg:checked:min-w-[72px]"
-          checked={index == currentTestimonial}
-          readOnly={true}
-        />
-      </>
-    );
+  function scrollToTestimonial(testimonial) {
+    const map = testimonialMap();
+    const node = map.get(testimonial);
+    node.scrollIntoView({
+      behavior: "smooth",
+      block: "nearest",
+      inline: "center",
+    });
   }
+
+  return (
+    <>
+      <style>{`:checked{background: ${theme}}`}</style>
+      <input
+        name="testimonials"
+        type="radio"
+        onClick={() => scrollToTestimonial(index)}
+        className="transition-all duration-300 ease-in-out size-2 bg-[#D9D9D9] rounded-full appearance-none hover:w-[15px] hover:checked:w-[23px] checked:w-[23px] md:size-3 md:hover:w-[30px] md:checked:w-[44px] md:checked:hover:w-[44px] lg:size-4 lg:hover:w-[55px] lg:checked:w-[72px] lg:checked:hover:w-[72px]"
+        checked={index == currentTestimonial}
+        readOnly={true}
+      />
+    </>
+  );
+}
